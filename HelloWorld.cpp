2f32f5b277ed4d7826c03353a26eaa4e0c9ef83d
Tim <1648746779@qq.com>
2020-10-19 11:36:41 +0800

    save edits

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include "WordSearch.hpp"
 
-int main() {
+void testWordSearch() {
 	std::vector<std::vector<char>> vec{
 		{'A', 'A', 'A', 'A'},
 		{'A', 'A', 'A', 'A'},
@@ -14,6 +14,11 @@ int main() {
     std::cout << ws.exist(vec, "AAAAAAAAAAAAAAAA") << std::endl;
     std::cout << ws.exist(vec, "SEE") << std::endl;
     std::cout << ws.exist(vec, "ABCB") << std::endl;
+}
+
+int main() {
+
+	testWordSearch();
 
 	return 0;
 }
